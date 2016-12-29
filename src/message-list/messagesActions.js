6bd46346e6aef90baf6506ad8345834d183ca3af
Boris Yankov <borisyankov@gmail.com>
2016-12-30 00:20:53 +0200

    Add Search

--- src/message-list/messagesActions.js
+++ src/message-list/messagesActions.js
@@ -4,6 +4,7 @@ import {
   MESSAGE_FETCH_START,
   MESSAGE_FETCH_SUCCESS,
 } from '../constants';
+import { searchNarrow } from '../utils/narrow';
 
 export const switchNarrow = (narrow) => ({
   type: SWITCH_NARROW,
@@ -29,5 +30,22 @@ export const fetchMessages = (
       anchor,
       narrow,
       startReached: numAfter === 0 && numBefore > messages.length,
+    });
+  };
+
+export const fetchSearchResults = (
+  auth,
+  query: string,
+) =>
+  async (dispatch) => {
+    const narrow = searchNarrow(query);
+    // dispatch({ type: MESSAGE_FETCH_START, narrow });
+
+    const messages = await getMessages(auth, Number.MAX_SAFE_INTEGER, 20, 0, narrow);
+
+    dispatch({
+      type: SEARCH_FETCH_SUCCESS,
+      auth,
+      messages,
     });
   };
