9c0ff2cbfb23fdc1ffc879d1054153ba07ecf18c
Boris Yankov <borisyankov@gmail.com>
2017-11-15 19:33:52 +0200

    Add topics autocomplete

---
+++ src/autocomplete/TopicAutocomplete.js
@@ -0,0 +1,52 @@
+/* @flow */
+import React, { PureComponent } from 'react';
+import { FlatList } from 'react-native';
+
+import type { User, GlobalState } from '../types';
+import connectWithActions from '../connectWithActions';
+import { getOwnEmail, getSortedUsers, getAutocompleteSuggestion } from '../selectors';
+import { Popup } from '../common';
+import UserItem from '../users/UserItem';
+
+type Props = {
+  filter: string,
+  onAutocomplete: (name: string) => void,
+  ownEmail: string,
+  users: User[],
+};
+
+class TopicAutocomplete extends PureComponent<Props> {
+  props: Props;
+
+  render() {
+    const { filter, ownEmail, users, onAutocomplete } = this.props;
+    const people: User[] = getAutocompleteSuggestion(users, filter, ownEmail);
+
+    if (people.length === 0) return null;
+
+    return (
+      <Popup>
+        <FlatList
+          keyboardShouldPersistTaps="always"
+          initialNumToRender={10}
+          data={people}
+          keyExtractor={item => item.email}
+          renderItem={({ item }) => (
+            <UserItem
+              fullName={item.fullName}
+              avatarUrl={item.avatarUrl}
+              email={item.email}
+              showEmail
+              onPress={() => onAutocomplete(item.fullName)}
+            />
+          )}
+        />
+      </Popup>
+    );
+  }
+}
+
+export default connectWithActions((state: GlobalState) => ({
+  ownEmail: getOwnEmail(state),
+  users: getSortedUsers(state),
+}))(TopicAutocomplete);
