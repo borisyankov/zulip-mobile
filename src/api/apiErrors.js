12eb02df88ed735b40c96d19aad096c5f8eb3d83
Boris Yankov <borisyankov@gmail.com>
2019-05-14 23:36:38 +0300

    isErrorBadEventQueueId

--- src/api/apiErrors.js
+++ src/api/apiErrors.js
@@ -11,3 +11,12 @@ export class EventQueueError extends Error {
     this.httpStatus = httpStatus;
   }
 }
+
+// if (e.message === 'API') {
+
+/**
+ * The event queue is too old or has been garbage collected.
+ * https://zulipchat.com/api/get-events-from-queue#bad_event_queue_id-errors
+ */
+export const isErrorBadEventQueueId = (e: EventQueueError): boolean =>
+  e.data !== undefined && e.data.code !== undefined && e.data.code === 'BAD_EVENT_QUEUE_ID';
